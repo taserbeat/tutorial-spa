105379513885b9306715c44ac03a1294f0d87325
taserbeat <taserbeat@gmail.com>
2020-10-20 23:05:49 +0900

    投票用のAPIを作成

--- tutorial/polls/api_views.py
+++ tutorial/polls/api_views.py
@@ -1,9 +1,25 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework import generics  # 追加
+from rest_framework.response import Response  # 追加
+from rest_framework.request import Request
 
-from .models import Question
-from .serializers import QuestionSerializer
+from .models import Question, Choice
+from .serializers import QuestionSerializer, ChoiceSerializer
 
 
 class QuestionViewSet(ReadOnlyModelViewSet):
     queryset = Question.objects.all()  # ここが対象となるレコードの指定．今回は全部
     serializer_class = QuestionSerializer  # 戻り値を定義したSerializer
+
+
+class VoteView(generics.CreateAPIView):
+    serializer_class = ChoiceSerializer
+
+    def post(self, request: Request, choice_id, *args, **kwargs):
+        obj = generics.get_object_or_404(queryset=Choice.objects.all(), id=choice_id)
+
+        obj.votes += 1
+        obj.save()
+
+        s = ChoiceSerializer(instance=obj)
+        return Response(s.data)
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include  # includeを追加
 
 from polls.api_urls import question_router  # 定義したquestion_routerをimport
+from polls import api_views as poll_views
 
 api_urlpatterns = [  # apiのURL一覧 (まだquestionだけ)
     path('questions/', include(question_router.urls)),  # 慣例として複数形にする
+    path('choices/<int:choice_id>/vote/', poll_views.VoteView.as_view()),
 ]
 
 urlpatterns = [
